8cc7655f55460aa28363b583c29209ab340046ab
anjali <anjalimalviya2532@gmail.com>
2022-05-11 21:48:41 +0530

    [anjali]Add: Comparing Two Line

--- LineComparisionProblem/LineComparisionProblem/Program.cs
+++ LineComparisionProblem/LineComparisionProblem/Program.cs
@@ -11,7 +11,7 @@ namespace LineComparisionProblem
             Console.WriteLine("... Welcome to Line Comparison Computation ... ");
             LineComparision lineCom = new LineComparision( 1,2,3,4);
             double length1=lineCom.LengthOfLine();
-            LineComparision line = new LineComparision(1, 2, 3, 4);
+            LineComparision line = new LineComparision(2, 6, 8, 10);
             double length2 = line.LengthOfLine();
             if(length1.Equals(length2))
             {
@@ -21,6 +21,12 @@ namespace LineComparisionProblem
             {
                 Console.WriteLine(" Lines Are Not Equal");
             }
+            if(length1.CompareTo(length2)==0)
+                Console.WriteLine("Both Lines Are Equal");
+            if (length1.CompareTo(length2) > 0)
+                Console.WriteLine("Line1 Is Greater Then Line2");
+            else
+                Console.WriteLine("Line1 Is Less Then Line2");
             Console.ReadKey();
         }
     }
